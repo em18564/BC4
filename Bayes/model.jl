630e87e5882a87453267a4d9ff7be79c76fa4374
em18564 <em18564@bristol.ac.uk>
2023-05-04 18:16:42 +0100

    changed model

--- Bayes/model.jl
+++ Bayes/model.jl
@@ -8,24 +8,37 @@ using StatsPlots
 using StatisticalRethinking
 
 NUM_SENTENCES = 205
-NUM_PARTICIPANTS = 24
+NUM_PARTICIPANTS = 4
 NUM_WORDS = 1931
 @model function model(participant,ERP,word,surprisal)
-    a1 ~ Normal(0,1)
-    a2 ~ MvNormal(zeros(NUM_PARTICIPANTS),1)
-    a3 ~ MvNormal(zeros(NUM_WORDS),1)
+    a ~ Normal(0,1)
+    a_p ~ MvNormal(zeros(NUM_PARTICIPANTS),1)
+    a_w ~ MvNormal(zeros(NUM_WORDS),1)
 
-    b1 ~ Normal(0,1)
-    b2 ~ MvNormal(zeros(NUM_PARTICIPANTS),1)
-    b3 ~ MvNormal(zeros(NUM_WORDS),1)
-    μ = (a1 .+ a2[Int.(participant)] .+ a3[Int.(word)]) .+ (b1 .+ b2[Int.(participant)] .+ b3[Int.(word)]) .* surprisal
-    σ ~ Uniform(0, 1)
-    ERP ~ MvNormal(μ, σ)
+    σ_a  ~ Exponential(1)
+    σ_ap ~ Exponential(1)
+    σ_aw ~ Exponential(1)
+
+    b ~ Normal(0,1)
+    b_p ~ MvNormal(zeros(NUM_PARTICIPANTS),1)
+    b_w ~ MvNormal(zeros(NUM_WORDS),1)
+
+    σ_b  ~ Exponential(1)
+    σ_bp ~ Exponential(1)
+    σ_bw ~ Exponential(1)
+
+
+    μ = ((a .* σ_a) .+ (a_p[Int.(participant)] .* σ_ap) .+ (a_w[Int.(word)] .* σ_aw)) .+ ((b .* σ_b) .+ (b_p[Int.(participant)] .* σ_bp) .+ (b_w[Int.(word)] .* σ_bw)) .* surprisal
+
+    ζ ~ Normal(0,1)
+    σ ~ Exponential(20)
+    
+    ERP = σ .* ζ .+ μ
 end
 
 df = CSV.read("savedData/df.csv", DataFrame)
-#precis(df)
-mod=model(df.Participant, df.ERP,df.word,df.surprisal)
+df_modified = subset(df, :Participant => ByRow(<(NUM_PARTICIPANTS+1)))
+mod=model(df_modified.Participant, df_modified.ERP,df_modified.word,df_modified.surprisal)
 m = sample(mod, NUTS(), MCMCThreads(), 1000, 4)
 m_df = DataFrame(m)
 CSV.write("savedData/m_df.csv", m_df)
