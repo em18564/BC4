fa8b44a2c92b6f67ad4440f21b0b9dbef580379f
em18564 <em18564@bristol.ac.uk>
2023-06-22 11:38:44 +0100

    fixed alt model maybe

--- Bayes/alternate_models/model_n400.jl
+++ Bayes/alternate_models/model_n400.jl
@@ -13,6 +13,8 @@ Pkg.add("Optim")
 Pkg.add("StatisticalRethinking")
 Pkg.add("MCMCDiagnosticTools")
 Pkg.add("Serialization")
+
+
 using Random
 using StatsBase
 using Distributions
@@ -31,10 +33,10 @@ using Serialization
 
 NUM_SENTENCES = 205
 NUM_PARTICIPANTS = 4
-NUM_WORDS = 1931
+NUM_WORDS = 800
 NUM_TYPES = 2
 NUM_ERP = 6 # ELAN, LAN, N400, EPNP, P600, PNP
-@model function model(participant,ERP,word,surprisal,tags,component)
+@model function model(participant,eRP,word,surprisal,tags,component)
     a   ~ MvNormal(zeros(NUM_WORDS),1)
     a_p ~ MvNormal(zeros(NUM_PARTICIPANTS),1)
     a_w ~ MvNormal(zeros(NUM_TYPES),1)
@@ -54,26 +56,21 @@ NUM_ERP = 6 # ELAN, LAN, N400, EPNP, P600, PNP
     σ_bp = 1
     σ_bw = 1
     σ_be = 1
-    
-    # my a is an a_i, my a_i is the word type
-    # contact sean, weds lunch pref 12 but can do 1
-    # plot regression on word type
-    # whisker plot
-    # for each variable take the relevant columns and plot them as violin plot/ HDI plot
-    # contact Davide for POS
 
-    μ = (((a[Int.(word)] .* σ_a) .+ (a_p[Int.(participant.+1)] .* σ_ap) .+ (a_w[Int.(tags.+1)] .* σ_aw) .+ (a_e .* σ_ae))
-     .+  ((b[Int.(word)] .* σ_b) .+ (b_p[Int.(participant.+1)] .* σ_bp) .+ (b_w[Int.(tags.+1)] .* σ_bw) .+ (b_e .* σ_be)) .* surprisal)
-
-    ζ ~ Normal(0,1)
-    σ ~ truncated(Cauchy(20),0,typemax(Int))
     
-    ERP = σ .* ζ .+ μ
+    σ ~ truncated(Cauchy(0,20),0,1000)
+    for i in eachindex(eRP)
+        #ζ ~ Normal(0,1)
+        μ = (((a[Int(word[i])] * σ_a) + (a_p[Int(participant[i]+1)] * σ_ap) + (a_w[Int(tags[i]+1)] * σ_aw) + (a_e * σ_ae))
+        +    ((b[Int(word[i])] * σ_b) + (b_p[Int(participant[i]+1)] * σ_bp) + (b_w[Int(tags[i]+1)] * σ_bw) + (b_e * σ_be)) * surprisal[i])
+        eRP[i] ~ Normal(μ,σ)
+      end
 end
 
 df = CSV.read("savedData/df_2.csv", DataFrame)
 df_modified_1 = subset(df, :Participant => ByRow(<(NUM_PARTICIPANTS)))
-df_modified = subset(df_modified_1, :component => ByRow(==(2)))
+df_modified_2 = subset(df_modified_1, :word => ByRow(<(NUM_WORDS)))
+df_modified = subset(df_modified_2, :component => ByRow(==(2)))
 mod=model(df_modified.Participant, df_modified.ERP,df_modified.word,df_modified.surprisal,df_modified.tags,df_modified.component)
 m = sample(mod, NUTS(), MCMCThreads(), 250,4)
 display(m)
