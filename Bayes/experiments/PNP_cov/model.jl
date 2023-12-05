99aec89e8fd2d1837d3eb4befb32a24fb5d20c28
em18564 <em18564@bristol.ac.uk>
2023-12-05 15:43:11 +0000

    added a bunch of stuff for loo

--- Bayes/experiments/PNP_cov/model.jl
+++ Bayes/experiments/PNP_cov/model.jl
@@ -30,25 +30,33 @@ using StatisticalRethinking
 
 using MCMCDiagnosticTools
 using Serialization
-
+using LinearAlgebra
 NUM_SENTENCES = 205
 NUM_PARTICIPANTS = 24
 NUM_WORDS = 1931
 NUM_TYPES = 2
 NUM_ERP = 6 # ELAN, LAN, N400, EPNP, P600, PNP
-@model function model(participant,word,surprisal,tags,ePNP)
-    a_w_s ~ filldist(Normal(0,1),NUM_TYPES)
-    b_w_s ~ filldist(Normal(0,0.5),NUM_TYPES)
-    a_w   = a_w_s[tags.+1]
-    b_w   = b_w_s[tags.+1]
+@model function model(participant,word,surprisal,tags,ePNP) #NGRAM surprisal LOO
+    σ_w ~ filldist(Exponential(), 2)
+    ρ_w ~ LKJ(2, 2) # LKJ prior on the correlation matrix
+    Σ_w = Symmetric(Diagonal(σ_w) * ρ_w * Diagonal(σ_w))
+    ab_w ~ filldist(MvNormal([0,0], Σ_w),NUM_TYPES)
+    a_w = ab_w[1,tags.+1]
+    b_w = ab_w[2,tags.+1]
+    
+    σ_p ~ filldist(Exponential(), 2)
+    ρ_p ~ LKJ(2, 2)
+    Σ_p = Symmetric(Diagonal(σ_p) * ρ_p * Diagonal(σ_p))
+    ab_p ~ filldist(MvNormal([0,0], Σ_p),NUM_PARTICIPANTS)
+    a_p = ab_p[1,participant.+1]
+    b_p = ab_p[2,participant.+1]
 
-    a_p_s ~ filldist(Normal(0,1),NUM_PARTICIPANTS)
-    b_p_s ~ filldist(Normal(0,0.5),NUM_PARTICIPANTS)
-    a_p   = a_p_s[participant.+1]
-    b_p   = b_p_s[participant.+1]
-
-    a_e ~ Normal(0,1)
-    b_e ~ Normal(0,0.5)
+    σ_e ~ filldist(Exponential(), 2)
+    ρ_e ~ LKJ(2, 2)
+    Σ_e = Symmetric(Diagonal(σ_e) * ρ_e * Diagonal(σ_e))
+    ab_e ~ filldist(MvNormal([0,0], Σ_e),1)
+    a_e = ab_e[1,1]
+    b_e = ab_e[2,1]
 
     μ = @. a_w + a_p + a_e + ((b_w + b_p + b_e) * surprisal)
 
