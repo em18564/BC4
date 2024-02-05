df4fa77660c663703ee35600b7689a4ecd322349
em18564 <em18564@bristol.ac.uk>
2024-02-05 15:23:38 +0000

    meeting with conor

--- Bayes/experiments/VI_PCA/PCI.jl
+++ Bayes/experiments/VI_PCA/PCI.jl
@@ -9,17 +9,18 @@ using DataFrames
 using Serialization
 using MultivariateStats
 using Plots
-using RDatasets
 df_full = CSV.read("../../input/dfHierarchicalNorm.csv", DataFrame)
 df = df_full[:, [:ELAN, :LAN, :N400, :EPNP, :P600, :PNP]]
+df_labels = Vector(df_full[:, :Tags])
 M = fit(PCA, transpose(Matrix(df)))
 Yte = predict(M, transpose(Matrix(df)))
 Xr = reconstruct(M, Yte)
 
 df_PCA = df_full
-
+#go through PCA in detail and get basic idea, interpret, plot by word type of scatter, check loo against normalisation.
 PCs = transpose(Yte)
-
+#ICA
+#scatter(Yte[1,:],Yte[2,:])
 df_PCA[!,"PC_1"] = PCs[:,1]
 df_PCA[!,"PC_2"] = PCs[:,2]
 df_PCA[!,"PC_3"] = PCs[:,3]
@@ -27,47 +28,53 @@ df_PCA[!,"PC_4"] = PCs[:,4]
 df_PCA[!,"PC_5"] = PCs[:,5]
 df_PCA[!,"PC_6"] = PCs[:,6]
 CSV.write("../../input/dfPCA.csv", df_PCA)
-ELAN = Yte[1,:]
-LAN  = Yte[2,:]
-N400 = Yte[3,:]
-EPNP = Yte[4,:]
-P600 = Yte[5,:]
-PNP  = Yte[6,:]
-p = scatter(ELAN[1,:],ELAN[2,:],ELAN[3,:],marker=:circle,linewidth=0,label="ELAN")
-scatter!(LAN[1,:],LAN[2,:],LAN[3,:],marker=:circle,linewidth=0,label="LAN")
-scatter!(N400[1,:],N400[2,:],N400[3,:],marker=:circle,linewidth=0,label="N400")
-scatter!(EPNP[1,:],EPNP[2,:],EPNP[3,:],marker=:circle,linewidth=0,label="EPNP")
-scatter!(P600[1,:],P600[2,:],P600[3,:],marker=:circle,linewidth=0,label="P600")
-scatter!(PNP[1,:],PNP[2,:],PNP[3,:],marker=:circle,linewidth=0,label="PNP")
 
-plot(p,xlabel="PC1",ylabel="PC2",zlabel="PC3")
+cont = Yte[:,df_labels.==0]
+func = Yte[:,df_labels.==1]
 
-p2 = scatter(ELAN[1,:],ELAN[2,:],marker=:circle,linewidth=0,label="ELAN")
-scatter!(LAN[1,:],LAN[2,:],marker=:circle,linewidth=0,label="LAN")
-scatter!(N400[1,:],N400[2,:],marker=:circle,linewidth=0,label="N400")
-scatter!(EPNP[1,:],EPNP[2,:],marker=:circle,linewidth=0,label="EPNP")
-scatter!(P600[1,:],P600[2,:],marker=:circle,linewidth=0,label="P600")
-scatter!(PNP[1,:],PNP[2,:],marker=:circle,linewidth=0,label="PNP")
+scatter(cont[1,:],cont[2,:])
+scatter!(func[1,:],func[2,:])
+# ELAN = Yte[1,:]
+# LAN  = Yte[2,:]
+# N400 = Yte[3,:]
+# EPNP = Yte[4,:]
+# P600 = Yte[5,:]
+# PNP  = Yte[6,:]
+# p = scatter(ELAN[1,:],ELAN[2,:],ELAN[3,:],marker=:circle,linewidth=0,label="ELAN")
+# scatter!(LAN[1,:],LAN[2,:],LAN[3,:],marker=:circle,linewidth=0,label="LAN")
+# scatter!(N400[1,:],N400[2,:],N400[3,:],marker=:circle,linewidth=0,label="N400")
+# scatter!(EPNP[1,:],EPNP[2,:],EPNP[3,:],marker=:circle,linewidth=0,label="EPNP")
+# scatter!(P600[1,:],P600[2,:],P600[3,:],marker=:circle,linewidth=0,label="P600")
+# scatter!(PNP[1,:],PNP[2,:],PNP[3,:],marker=:circle,linewidth=0,label="PNP")
 
-plot(p2,xlabel="PC1",ylabel="PC2")
+# plot(p,xlabel="PC1",ylabel="PC2",zlabel="PC3")
 
-p3 = scatter(ELAN[1,:],ELAN[3,:],marker=:circle,linewidth=0,label="ELAN")
-scatter!(LAN[1,:],LAN[3,:],marker=:circle,linewidth=0,label="LAN")
-scatter!(N400[1,:],N400[3,:],marker=:circle,linewidth=0,label="N400")
-scatter!(EPNP[1,:],EPNP[3,:],marker=:circle,linewidth=0,label="EPNP")
-scatter!(P600[1,:],P600[3,:],marker=:circle,linewidth=0,label="P600")
-scatter!(PNP[1,:],PNP[3,:],marker=:circle,linewidth=0,label="PNP")
+# p2 = scatter(ELAN[1,:],ELAN[2,:],marker=:circle,linewidth=0,label="ELAN")
+# scatter!(LAN[1,:],LAN[2,:],marker=:circle,linewidth=0,label="LAN")
+# scatter!(N400[1,:],N400[2,:],marker=:circle,linewidth=0,label="N400")
+# scatter!(EPNP[1,:],EPNP[2,:],marker=:circle,linewidth=0,label="EPNP")
+# scatter!(P600[1,:],P600[2,:],marker=:circle,linewidth=0,label="P600")
+# scatter!(PNP[1,:],PNP[2,:],marker=:circle,linewidth=0,label="PNP")
 
-plot(p3,xlabel="PC1",ylabel="PC3")
+# plot(p2,xlabel="PC1",ylabel="PC2")
 
-p4 = scatter(ELAN[2,:],ELAN[3,:],marker=:circle,linewidth=0,label="ELAN")
-scatter!(LAN[2,:],LAN[3,:],marker=:circle,linewidth=0,label="LAN")
-scatter!(N400[2,:],N400[3,:],marker=:circle,linewidth=0,label="N400")
-scatter!(EPNP[2,:],EPNP[3,:],marker=:circle,linewidth=0,label="EPNP")
-scatter!(P600[2,:],P600[3,:],marker=:circle,linewidth=0,label="P600")
-scatter!(PNP[2,:],PNP[3,:],marker=:circle,linewidth=0,label="PNP")
+# p3 = scatter(ELAN[1,:],ELAN[3,:],marker=:circle,linewidth=0,label="ELAN")
+# scatter!(LAN[1,:],LAN[3,:],marker=:circle,linewidth=0,label="LAN")
+# scatter!(N400[1,:],N400[3,:],marker=:circle,linewidth=0,label="N400")
+# scatter!(EPNP[1,:],EPNP[3,:],marker=:circle,linewidth=0,label="EPNP")
+# scatter!(P600[1,:],P600[3,:],marker=:circle,linewidth=0,label="P600")
+# scatter!(PNP[1,:],PNP[3,:],marker=:circle,linewidth=0,label="PNP")
 
-plot(p4,xlabel="PC2",ylabel="PC3")
+# plot(p3,xlabel="PC1",ylabel="PC3")
+
+# p4 = scatter(ELAN[2,:],ELAN[3,:],marker=:circle,linewidth=0,label="ELAN")
+# scatter!(LAN[2,:],LAN[3,:],marker=:circle,linewidth=0,label="LAN")
+# scatter!(N400[2,:],N400[3,:],marker=:circle,linewidth=0,label="N400")
+# scatter!(EPNP[2,:],EPNP[3,:],marker=:circle,linewidth=0,label="EPNP")
+# scatter!(P600[2,:],P600[3,:],marker=:circle,linewidth=0,label="P600")
+# scatter!(PNP[2,:],PNP[3,:],marker=:circle,linewidth=0,label="PNP")
+
+# plot(p4,xlabel="PC2",ylabel="PC3")
 
 
 # iris = dataset("datasets", "iris")
