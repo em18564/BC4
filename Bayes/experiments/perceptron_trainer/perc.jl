278a18bbfd4de65a93023030889689a62a94f805
em18564 <em18564@bristol.ac.uk>
2024-03-14 17:51:52 +0000

    flux stuff

--- Bayes/experiments/perceptron_trainer/perc.jl
+++ Bayes/experiments/perceptron_trainer/perc.jl
@@ -4,12 +4,15 @@ using Serialization,CSV,DataFrames,StatsBase,Distributions,Plots,Random,StatsFun
 df     = CSV.read("../../input/dfPCANorm.csv", DataFrame)
 dfTags = CSV.read("../../input/full_tags.csv", DataFrame)
 dfWord = filter(row -> row.Participant==0, df)[:,"Word"]
+lr = 0.01
 
 function lookup(x)
     return findfirst(item -> item == x, dfWord)
 end
 
-
+function mse(preds,targets)
+    return 0.5*(sum(preds-targets)^2)
+end
 
 dfTags = filter(row -> row.Participant==0, dfTags)
 dfTags = dfTags[!,"tags"]
@@ -35,30 +38,46 @@ end
 function forward(xs)
     z   = encoder[xs,:]*W1
     h   = logistic.(z)
-    v   = h*W2
-    out = logistic.(v)
-    return z,h,v,out
+    out = h*W2
+    return z,h,out
 end
 
-function forward2(ys)
-    z = ys * transpose(W2)
-    h = logistic.(z)
-    v = h * transpose(W1)
-    out= logistic.(v)
-    return z,h,v,out
+function dSigmoid_dA(a)
+    return logistic.(a).*(1 .-logistic.(a))
 end
 
-function runModel(batchSize,epochs,iterations)
+function backprop(xs,z,h,preds,targets)
+    dLoss_dP     = preds - targets
+    dLoss_dW2    = transpose(h) * dLoss_dP
+    dLoss_dH     = dLoss_dP * transpose(W2)
+    dS_dZ        = dSigmoid_dA(z)
+    dLoss_dZ     = dS_dZ .* dLoss_dH
+    dLoss_dW1    = transpose(xs) * dLoss_dZ
+    return dLoss_dW1,dLoss_dW2
+end
+
+
+
+function learnW2()
+    inp = Xtr[1:100]
+    labels = Ytr[1:100,:]
+    z,h,v,out = forward(inp)
+    dLoss_dW2 = backprop(inp,z,h,v,out,labels)
+    global W2,lr
+    W2 = W2 .- (lr * dLoss_dW2)
+    return(mse(out,labels))
+end
+function runModel(batchSize,iterations)
+    global W1,W2,lr
+
     for i in 1:iterations
-        for j in 1:epochs
-            sample = shuffle(1:length(Xtr[:,1]))[1:batchSize]
-            z,h,v,out = forward(Xtr[sample])
-            println("forward-"*string(i) * "-" *string(j))
-        end
-        for j in 1:epochs
-            sample = shuffle(1:length(Xtr[:,1]))[1:batchSize]
-            z,h,v,out = forward2(Ytr[sample,:])
-            println("backward-"*string(i) * "-" *string(j))
-        end
+        sample = shuffle(1:length(Xtr[:,1]))[1:batchSize]
+        inp = Xtr[sample]
+        labels = Ytr[sample,:]
+        z,h,out = forward(inp)
+        dLoss_dW1,dLoss_dW2 = backprop(inp,z,h,out,labels)
+        W1 = W1 .- (lr * dLoss_dW1)
+        W2 = W2 .- (lr * dLoss_dW2)
+        println("forward-"*string(i) * "-" *string(j) * ":" * string(mse(out,labels)))
     end
 end
