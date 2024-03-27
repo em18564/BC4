3fd2613214b373573423125c2ed4e73dfd9584a0
em18564 <em18564@bristol.ac.uk>
2024-03-27 11:11:45 +0000

    changed order of eeg

--- Bayes/dataExtracting/EEG.jl
+++ Bayes/dataExtracting/EEG.jl
@@ -24,7 +24,7 @@ for i in 10:24
     push!(dicts,dict)
 end
 
-df = DataFrame( Participant = Integer[], Tag = Integer[], Word = Integer[],
+df = DataFrame( Participant = Integer[], Tag = Integer[], Word = String[],
                 EEG1=Float64[], EEG2=Float64[], EEG3=Float64[], EEG4=Float64[], EEG5=Float64[], EEG6=Float64[], EEG7=Float64[], EEG8=Float64[], EEG9=Float64[], EEG10=Float64[],
                 EEG11=Float64[],EEG12=Float64[],EEG13=Float64[],EEG14=Float64[],EEG15=Float64[],EEG16=Float64[],EEG17=Float64[],EEG18=Float64[],EEG19=Float64[],EEG20=Float64[], 
                 EEG21=Float64[],EEG22=Float64[],EEG23=Float64[],EEG24=Float64[],EEG25=Float64[],EEG26=Float64[],EEG27=Float64[],EEG28=Float64[],EEG29=Float64[],EEG30=Float64[], 
@@ -34,9 +34,21 @@ for (i,d) in pairs(dicts)
     events = d["event"]["latency"][:]
     types  = d["event"]["type"][:]
     data   = d["data"]
+    currentSent = -1
+    currentWord = ""
     tags   = (subset(df_old, :Participant => ByRow(==(i-1))))[!,:Tags]
-    words  = (subset(df_old, :Participant => ByRow(==(i-1))))[!,:Word]
-    for (j,w) in pairs(words)
+    for (w,j) in pairs(types)
+        if i!=6 && !(i==14 && w<4) && !(i==20 && w<5)
+            println(string(i) * ":" * string(w) * " " * string(j))
+            if Integer(j)>50
+                currentSent=(Integer(j)-50)
+                currentWord=stim[currentSent][1]
+            else
+                currentWord=stim[currentSent][Integer(j)]
+            end
+
+        end
+        
         if i == 9
             global limit = 1917
         elseif i == 13
@@ -53,19 +65,33 @@ for (i,d) in pairs(dicts)
             if w < 1420
                 lat  = Integer(events[Integer(w)])
                 EEGs = data[:,lat:lat+174]
-
+                if Integer(j)>50
+                    currentSent=(Integer(j)-50)
+                    currentWord=stim[currentSent][1]
+                else
+                    currentWord=stim[currentSent][Integer(j)]
+                end
 
 
 
                 for k in 1:(length(EEGs[1,:]))
-                    push!(df, vcat(i,tags[j],Integer(w),EEGs[:,k]))
+                    push!(df, vcat(i,tags[Integer(j)],currentWord,EEGs[:,k]))
                 end
-            elseif w<1833
+            elseif w<1833 
                 lat  = Integer(dictPart2["event"]["latency"][Integer(w)-1419])
+                typ2 = Integer(dictPart2["event"]["type"][Integer(w)-1419])
                 data = dictPart2["data"]
                 EEGs = data[:,lat:lat+174]
+                if typ2>50
+                    currentSent=Integer(typ2-50)
+                    currentWord=stim[currentSent][1]
+                else
+                    currentWord=stim[currentSent][typ2]
+                end
+                println(string(w-1419) * ":" * string(typ2) * "-" *string(currentSent) * " " * currentWord)
+
                 for k in 1:(length(EEGs[1,:]))
-                    push!(df, vcat(i,tags[j],Integer(w),EEGs[:,k]))
+                    push!(df, vcat(i,tags[Integer(j)],currentWord,EEGs[:,k]))
                 end            
             end
         else
@@ -73,7 +99,7 @@ for (i,d) in pairs(dicts)
                 lat  = Integer(events[Integer(w)])
                 EEGs = data[:,lat:lat+174]
                 for k in 1:(length(EEGs[1,:]))
-                    push!(df, vcat(i,tags[j],Integer(w),EEGs[:,k]))
+                    push!(df, vcat(i,tags[Integer(j)],currentWord,EEGs[:,k]))
                 end
                 
             end
