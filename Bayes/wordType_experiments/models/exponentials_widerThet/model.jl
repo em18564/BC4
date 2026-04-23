c8a5fc9cb0cab8ba5419589da1640bc3ddcbad51
em18564 <em18564@bristol.ac.uk>
2026-04-23 21:54:14 +0100

    a

---
+++ Bayes/wordType_experiments/models/exponentials_widerThet/model.jl
@@ -0,0 +1,60 @@
+include("../../typeStructures.jl")
+include("../../model_master.jl")
+include("../../setup.jl")
+include("../../plottingFuncs.jl")
+
+@model function model(participant,word,surprisal,tags,PCA)
+
+  σ_aw ~ Exponential()
+  a_ws ~ filldist(Normal(0, 1),NUM_TYPES)
+  σ_bw ~ Exponential()
+  b_ws ~ filldist(Normal(0, 1),NUM_TYPES)
+  a_w = a_ws[tags.+1]
+  b_w = b_ws[tags.+1]
+
+  σ_ap ~ Exponential()
+  a_ps ~ filldist(Normal(0, 1),NUM_PARTICIPANTS)
+  σ_bp ~ Exponential()
+  b_ps ~ filldist(Normal(0, 1),NUM_PARTICIPANTS)
+  a_p = a_ps[participant.+1]
+  b_p = b_ps[participant.+1]
+
+  a_e  ~ Normal(0,1)
+  b_e  ~ Normal(0,1)
+
+  μ = @. a_w*σ_aw + a_p*σ_ap + a_e + ((b_w*σ_bw + b_p*σ_bp + b_e) * surprisal)
+
+  σ ~ truncated(Cauchy(0., 5.); lower = 0)
+  for i in eachindex(PCA)
+    PCA[i] ~ Normal(μ[i],σ)
+  end
+end
+
+
+
+df_modified, dfPCA, pc, NUM_PARTICIPANTS,  NUM_WORDS, TYPE_STRUCTURE, NUM_TYPES,wordTypes,cols = createVariables()
+output_loc = "output_" * TYPE_STRUCTURE * "_" * string(NUM_PARTICIPANTS) * "_" * string(NUM_WORDS)
+if (!isdir(output_loc))
+  mkdir(output_loc)
+end
+
+mod=model(df_modified.Participant,df_modified.Word,df_modified.Surprisal,df_modified.fullTag,dfPCA[:,pc])
+#m = sample(mod, NUTS(), MCMCThreads(), 250,4)
+#display(m)
+#serialize(output_loc*"/out"*string(pc)*".jls",m)
+
+
+if(pc == 1)
+  #wait for all other PCs to finish before greating plots
+  is_waiting = true
+  while(is_waiting)
+    if(isfile(output_loc*"/out1.jls") && isfile(output_loc*"/out2.jls") && isfile(output_loc*"/out3.jls") && isfile(output_loc*"/out4.jls"))
+      plotGraphs(output_loc,wordTypes,cols) 
+      is_waiting=false
+    else
+      print("Waiting for other PCs to complete")
+      sleep(30)
+    end
+    
+  end
+end
