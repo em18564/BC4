a793c1e4c7e7b372245256088d509b4c14ad709c
em18564 <em18564@bristol.ac.uk>
2026-05-18 15:46:20 +0100

    a

---
+++ Bayes/wordType_experiments/models/8_tightCauchy_e_1/model.jl
@@ -0,0 +1,41 @@
+include("../../typeStructures.jl")
+include("../../model_master.jl")
+include("../../plottingFuncs.jl")
+include("../../setup.jl")
+
+@model function model(participant,word,surprisal,tags,PCA,ExpMean,cauchyMean)
+
+  σ_aw ~ Exponential(1)
+  a_ws ~ filldist(Normal(0, 1),NUM_TYPES)
+  σ_bw ~ Exponential(1)
+  b_ws ~ filldist(Normal(0, 1),NUM_TYPES)
+  a_w = a_ws[tags.+1]
+  b_w = b_ws[tags.+1]
+
+  σ_ap ~ Exponential(1)
+  a_ps ~ filldist(Normal(0, 1),NUM_PARTICIPANTS)
+  σ_bp ~ Exponential(1)
+  b_ps ~ filldist(Normal(0, 1),NUM_PARTICIPANTS)
+  a_p = a_ps[participant.+1]
+  b_p = b_ps[participant.+1]
+
+  a_e  ~ Normal(0,1)
+  b_e  ~ Normal(0,1)
+
+    μ = @. a_w*σ_aw + a_p*σ_ap + a_e + ((b_w*σ_bw + b_p*σ_bp + b_e) * surprisal)
+
+  σ ~ truncated(Cauchy(0., 0.5); lower = 0)
+
+  for i in eachindex(PCA)
+    PCA[i] ~ Normal(μ[i],σ)
+  end
+  
+end
+
+
+timeStart = now()
+df_modified, dfPCA, pc, NUM_PARTICIPANTS,  NUM_WORDS, TYPE_STRUCTURE, NUM_TYPES,wordTypes,cols,isPlotting,analyseEssRhat,output_loc,expMean,cauchyMean = createVariables()
+mod=model(df_modified.Participant,df_modified.Word,df_modified.Surprisal,df_modified.fullTag,dfPCA[:,pc],expMean,cauchyMean)
+runModel(mod,df_modified, dfPCA, pc, NUM_PARTICIPANTS,  NUM_WORDS, TYPE_STRUCTURE, NUM_TYPES,wordTypes,cols,isPlotting,analyseEssRhat,output_loc,expMean,cauchyMean)
+
+print("\ntime taken", now()-timeStart)
---
+++ Bayes/wordType_experiments/testing/essRhatBarChart.jl
@@ -0,0 +1,132 @@
+# %%
+
+include("../setup.jl")
+include("../typeStructures.jl")
+include("../model_master.jl")
+include("../plottingFuncs.jl")
+using SkipNan
+
+# %%
+
+
+outputDirs=["1_baseModel", "2_baseModel_lowerCauchy", "3_baseModel_noLKJintercept",
+            "4_baseModel_noLKJ","5_baseModel_0.5","6_baseModel_renormalised",
+            "7_baseModel_0.25"]
+function getessRhats(outputName)
+    outputDir = "models/"*outputName*"/output_FullCF_23_1931"
+    chn1 = deserialize(outputDir*"/out1.jls")
+    chn2 = deserialize(outputDir*"/out2.jls")
+    chn3 = deserialize(outputDir*"/out3.jls")
+    chn4 = deserialize(outputDir*"/out4.jls")
+
+    ss_df1  = DataFrames.DataFrame(summarystats(chn1))
+    ss_df2  = DataFrames.DataFrame(summarystats(chn2))
+    ss_df3  = DataFrames.DataFrame(summarystats(chn3))
+    ss_df4  = DataFrames.DataFrame(summarystats(chn4))
+    ess  = hcat(ss_df1.ess_bulk,   ss_df2.ess_bulk,    ss_df3.ess_bulk,    ss_df4.ess_bulk)
+    rhat = hcat(ss_df1.rhat,       ss_df2.rhat,        ss_df3.rhat,        ss_df4.rhat)
+    return ess,rhat
+end
+
+
+allEss  = []
+allRhat = []
+for od in outputDirs
+    ess,rhat = getessRhats(od)
+    push!(allEss,ess)
+    push!(allRhat,rhat)
+end
+
+
+# %%
+function getBar(i, xbase, mean, max, min)
+
+    offset = (-0.3, -0.1,0.1, 0.3)[i] 
+
+    bar = PlotlyJS.bar(
+        x = xbase,
+        y = mean,
+        name = "PC " * string(i),
+
+        error_y = attr(
+            type = "data",
+            symmetric = false,
+            array = max,
+            arrayminus = min,
+            visible = true
+        )
+    )
+
+    labels1 = PlotlyJS.scatter(
+        x = xbase .+ offset,
+        y = mean .+ max .*1.01,
+        mode = "text",
+        text = round.(mean .+ max, digits=2),
+        textposition = "top center",
+        showlegend = false,
+        textfont = attr(
+        size = 8
+        )
+    )
+    labels2 = PlotlyJS.scatter(
+        x = xbase .+ offset,
+        y = (mean .- min).-((mean).*0.07).+0.04,
+        mode = "text",
+        text = round.(mean .- min, digits=2),
+        textposition = "top center",
+        showlegend = false,
+        textfont = attr(
+        size = 8
+    )
+    )
+
+    return bar, labels1,labels2
+end
+
+
+function barPlot(data,mytitle)
+    means = zeros((7,4))
+    maxs  = zeros((7,4))
+    mins  = zeros((7,4))
+    for i in range(1,7)
+        for j in range(1,4)
+            curData=skipnan(data[i][:,j])
+            means[i,j] = mean(curData)
+            maxs[i,j]  = maximum(curData)-mean(curData)
+            mins[i,j]  = mean(curData)-minimum(curData)
+        end     
+    end
+    xbase = 1:length(outputDirs)
+
+    bars = [
+        getBar(i, xbase, means[:,i], maxs[:,i], mins[:,i])
+        for i in 1:4
+    ]
+
+    layout = Layout(
+        barmode = "group",
+        xaxis = attr(
+            tickvals = xbase,
+            ticktext = outputDirs
+        ),
+        title=mytitle
+    )
+
+    PlotlyJS.plot(
+        [
+            bars[1][1], bars[1][2], bars[1][3],
+            bars[2][1], bars[2][2], bars[2][3],
+            bars[3][1], bars[3][2], bars[3][3],
+            bars[4][1], bars[4][2], bars[4][3]
+        ],
+        layout,
+    )
+end
+
+
+p = barPlot(allEss,"ess Average scores for different models (with min and max error bars)")
+PlotlyJS.savefig(p,"figs/modelComparison/ess.png",
+        width=1400, height=800)
+p = barPlot(allRhat,"rhat Average scores for different models (with min and max error bars)")
+PlotlyJS.savefig(p,"figs/modelComparison/rhat.png",
+        width=1400, height=800)
