749aba136855a248cd7399952b2cb9235c943714
em18564 <em18564@bristol.ac.uk>
2026-05-21 15:59:59 +0100

    a

--- Bayes/wordType_experiments/models/9_partial_pooling/model.jl
+++ Bayes/wordType_experiments/models/9_partial_pooling/model.jl
@@ -9,27 +9,27 @@ include("../../setup.jl")
   a_wts ~ filldist(Normal(0, 1),NUM_TYPES)
   σ_bwt ~ Exponential(0.5)
   b_wts ~ filldist(Normal(0, 1),NUM_TYPES)
-  a_wt = a_wts[tags.+1]
-  b_wt = b_wts[tags.+1]
+  a_wt = a_wts[tags.+1].* σ_awt
+  b_wt = b_wts[tags.+1].* σ_bwt
 
   σ_aw ~ Exponential(0.5)
-  a_ws ~ filldist(Normal(a_wt*σ_awt, 1),NUM_TYPES)
   σ_bw ~ Exponential(0.5)
-  b_ws ~ filldist(Normal(b_wt*σ_bwt, 1),NUM_TYPES)
-  a_w = a_ws[word.+1]
-  b_w = b_ws[word.+1]
+  a_ws ~ filldist(Normal(0, 1), NUM_UNIQUE_WORDS)
+  b_ws ~ filldist(Normal(0, 1), NUM_UNIQUE_WORDS)
+  a_w = a_ws[word.+1].*σ_aw
+  b_w = b_ws[word.+1].*σ_bw
 
   σ_ap ~ Exponential(0.5)
   a_ps ~ filldist(Normal(0, 1),NUM_PARTICIPANTS)
   σ_bp ~ Exponential(0.5)
   b_ps ~ filldist(Normal(0, 1),NUM_PARTICIPANTS)
-  a_p = a_ps[participant.+1]
-  b_p = b_ps[participant.+1]
+  a_p = a_ps[participant.+1].*σ_ap
+  b_p = b_ps[participant.+1].*σ_bp
 
   a_e  ~ Normal(0,0.5)
   b_e  ~ Normal(0,0.5)
 
-    μ = @. a_w*σ_aw + a_p*σ_ap + a_e + ((b_w*σ_bw + b_p*σ_bp + b_e) * surprisal)
+    μ = @. a_wt + a_w + a_p + a_e + ((b_wt + b_w + b_p + b_e) * surprisal)
 
   σ ~ truncated(Cauchy(0., 1.); lower = 0)
 
