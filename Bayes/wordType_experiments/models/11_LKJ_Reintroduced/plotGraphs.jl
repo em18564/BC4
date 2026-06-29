e3dc359fdd01cb66e5780190cece6225919cf33f
em18564 <em18564@bristol.ac.uk>
2026-06-29 18:12:34 +0100

    Update plotGraphs.jl

--- Bayes/wordType_experiments/models/11_LKJ_Reintroduced/plotGraphs.jl
+++ Bayes/wordType_experiments/models/11_LKJ_Reintroduced/plotGraphs.jl
@@ -5,7 +5,7 @@ include("../../plottingFuncs.jl")
 
 
 
-df_modified, dfPCA, pc, NUM_PARTICIPANTS,  NUM_WORDS, TYPE_STRUCTURE, NUM_TYPES,wordTypes,cols = createVariables()
+df_modified, dfPCA, pc, NUM_PARTICIPANTS,  NUM_WORDS, TYPE_STRUCTURE, NUM_TYPES,wordTypes,cols,noPCA = createVariables()
 
 output_loc = "output_" * TYPE_STRUCTURE * "_" * string(NUM_PARTICIPANTS) * "_" * string(NUM_WORDS)
 
@@ -13,9 +13,9 @@ if(pc == 1)
   #wait for all other PCs to finish before greating plots
   global is_waiting = true
   while(is_waiting)
-    if(isfile(output_loc*"/out1.jls") && isfile(output_loc*"/out2.jls") && isfile(output_loc*"/out3.jls") && isfile(output_loc*"/out4.jls"))
+    if(reduce(&,[isfile(output_loc*"/out"*string(i)*".jls") for i in range(1,noPCA)]))
       print("plotting graphs")
-      plotGraphs(output_loc,wordTypes,cols) 
+      plotGraphs(output_loc,wordTypes,cols,noPCA) 
       global is_waiting=false
     else
       print("Waiting for other PCs to complete")
