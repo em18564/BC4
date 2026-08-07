6de7bf473108ad95b3134cc11472379879a957d5
em18564 <em18564@bristol.ac.uk>
2026-08-07 14:45:23 +0100

    a

--- Bayes/wordType_experiments/models/testingDifferentPCS/plotGraphs.jl
+++ Bayes/wordType_experiments/models/testingDifferentPCS/plotGraphs.jl
@@ -1,6 +1,10 @@
+# %%
 include("../../typeStructures.jl")
-include("../../model_master.jl")
 include("../../setup.jl")
+# %%
+include("../../model_master.jl")
 include("../../plottingFuncs.jl")
+# %%
 
-plotExistingModelGraphs()
+
+plotExistingModelGraphs(["1", "6", "23", "1931", "NoNum", "11", "2", "1", "1", "1", "1"])
