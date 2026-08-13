1aa79ebc6c3f3dd29d56fc3de4bb365890fdfd12
em18564 <em18564@bristol.ac.uk>
2026-08-13 12:17:00 +0100

    a

--- Bayes/wordType_experiments/extractJuliJLS.jl
+++ Bayes/wordType_experiments/extractJuliJLS.jl
@@ -21,11 +21,11 @@ include("plottingFuncs.jl")
 # 8 Pron
 # 9 Prt
 # 10 Verb
-outputDirs =[   "models/testingDifferentPCS/output_FullADP_23_1931_6PCA_250",
-                "models/testingDifferentPCS/output_FullADP_23_1931_6PCA_1000",
-                "models/testingDifferentPCS/output_NoNum_23_1931_6PCA_250",
-                "models/testingDifferentPCS/output_NoNum_23_1931_6PCA_1000",
-                "models/testingDifferentPCS/output_FullADP_23_1931_NOPCA"]          
+outputDirs =[   "models/sampledSigmas/output_FullADP_23_1931_6PCA_250",
+                "models/sampledSigmas/output_NoNum_23_1931_6PCA_250", 
+                "models/wordLen/output_FullADP_23_1931_6PCA_250",
+                "models/wordLen/output_NoNum_23_1931_6PCA_250"
+]         
 
 function extractFiles(outputDir)
     chn1 = deserialize(outputDir*"/out1.jls")
