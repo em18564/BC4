0a546ab38eb20faa1b846d7bc14d6093eeec6795
em18564 <em18564@bristol.ac.uk>
2026-09-14 12:50:33 +0100

    aa

--- Bayes/wordType_experiments/extractJuliJLS.jl
+++ Bayes/wordType_experiments/extractJuliJLS.jl
@@ -24,7 +24,6 @@ include("plottingFuncs.jl")
 outputDirs =[
                 "models/2_HalfNormals/output_NoNum_23_1931_6PCA_250", 
                 "models/3_participantSigmas/output_NoNum_23_1931_6PCA_250", 
-                "models/4_partialPooled/output_NoNum_23_1931_6PCA_250", 
                 "models/5_noPartA/output_NoNum_23_1931_6PCA_250",
                 "models/6_LKJPart/output_NoNum_23_1931_6PCA_250"
 ]         
